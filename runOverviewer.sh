501b950920cbeafcb54cf328a24be30757dc3694
Josh Wickham <josh@i-josh.com>
2018-09-18 09:16:43 -0700

    updated configuration and runscripts

--- runOverviewer.sh
+++ runOverviewer.sh
@@ -4,9 +4,10 @@ st=$(date)
 logfile=/minecraft/overviewer.log
 configfile=/minecraft/overviewer.cfg
 
-echo "run starting at ${st}" >> ${logfile} 
-overviewer.py --config=${configfile} >> ${logfile}
-#overviewer.py --config=${configfile} --genpoi >> ${logfile}
+echo "run starting at ${st}" >> ${logfile}
+cd /tmp/overviewer
+python overviewer.py --config=/minecraft/overviewer.cfg | tee /minecraft/overviewer.log
+#python overviewer.py --config=/minecraft/overviewer.cfg --genpoi | tee /minecraft/overviewer.log
 et=$(date)
 echo "run ended at ${et}" >> ${logfile}
 echo "" >> ${logfile}
